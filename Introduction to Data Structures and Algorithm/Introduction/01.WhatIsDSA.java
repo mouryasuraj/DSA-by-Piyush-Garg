1e312e2c10ea1313b48aba651e2539be75c80343
Suraj <mouryasuraj6266444@gmail.com>
2024-01-15 20:52:56 +0530

    COmpleted: Intorduction to DSA, Space and Time complexity

---
+++ Introduction to Data Structures and Algorithm/Introduction/01.WhatIsDSA.java
@@ -0,0 +1,31 @@
+/*
+ 
+1.DSA stands for Data Structure & Algorithm.
+2.Data Structure:
+    a.Data Structure is used to Structure your data in a certain way.
+    b.Data Structure is like different ways to store and organize data so that we can easily find them and perform operations quickly.
+    c.example: Think of a list of your favorite books. You can organize them in different ways. Alphabetically, by genre or by the date of you read them. Each of these arrangements are different data structure.
+    d.Another example: 
+        1.Suppose, you have a telephone diary and you want to search phone number for "Suraj" and the records are arranged in random order. 
+        2.So, we have to check every record in the diary and it will take longer much time to find.
+        3.But, imagine that if the record is arranged in alphabetical order then it become easy to find the data.
+        4.You just just to the  first letter of the name like "S" and then "U" and  within a minute you will find the phone number.
+        5.So, that's why structuring data is important. It takes less time to find the data.
+    e.Types of Data Structure:
+        1.Linear Data Structure:
+            a.In linear data structure, data is arranged in a linear or sequential order.
+            b.Ex: Arrays, LinkedList, Stacks and Queues.
+            c.Linear struture traversed in a single, straight lind, visiting each element one by one.
+            d.It is easy to understand and utilize memory efficiently.
+            e.Insertion and deletion at arbitrary poisition is inefficiently. 
+        2.Non-linear Data Structure:
+            a.In non linear structure, data is not arranged in a linear or sequential order. It create more complex relationships.
+            b.Ex: Graphs, Tree.
+
+3.Algorithm:
+    a.Algorithm is used to perform the best approach so that the operation should take less time.
+    b.If we take the above example, We have to perform the algorithm to find the phone number in the best way.
+    c.The correct algorithm is, first it will search for first letter "S" and then "U" and after that you will find the name "Suraj"
+
+    
+*/
---
+++ Introduction to Data Structures and Algorithm/Introduction/02.TimeAndSpaceComplexity.java
@@ -0,0 +1,35 @@
+/*
+
+1.Time Complexity:
+    a.Time complexity is a measure of how the running time of algorithm increase with the inputs.
+    b.It helps us to understand that how efficient an algorithm is in terms of time as the input grows larger.
+    c.We use Big O notation to express time complexity. Big O provides an upper bound on the growth rate of the running time.
+    d.There are 3 ways to express time complexity:
+        a.Big-O (Mostly used) because it gives an upper bound or worst case of how the running time or space requirement of an algorithm grow as input size increases.
+            1.Constant time - O(1)
+            2.Linear time - O(n)
+            3.Log time - O(log n)
+            4.Quadratic time- O(n^2)
+            5.Cubic time- O(n^3)
+        b.Theta-O
+        c.Omega-O
+
+2.Space complexity:
+    a.Space complexity is a measure of how much memroy algorithm need in relation to the size of  input processess.
+    b.It helps us to understand how the memory requirement of an algorithm grow as the input size increases.
+            1.Constant space - O(1)
+            2.Linear space - O(n)
+            3.Quadratic space- O(n^2)
+
+ */
+
+//  Examples
+// Constant - O(1)
+
+public class Main{
+    public static void main(String args[]){
+        // Big O(1) - Constant
+        int[] arr = new int[]{1,2,3,4,5};
+        System.out.println(arr[0]);    //O(1), Here big O is constant because If length of the array goes to one million items or just one item it will take constant time to give output.
+    }
+}
